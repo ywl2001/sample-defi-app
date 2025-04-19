a9a0041d4ed569ce6fdf1b5076575830264ad7fd
Ya Wen Liu <nina906266628@gmail.com>
2025-04-19 18:00:37 +0800

    Updates

---
+++ precache-manifest.fe19c8ce2c8ee0c120e5ca924eec426a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362355134502da45290996c2c2259ecd",
+    "url": "/sample-defi-app/index.html"
+  },
+  {
+    "revision": "38af8bdbc21557fdf598",
+    "url": "/sample-defi-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9761c3672df8438e2648",
+    "url": "/sample-defi-app/static/js/2.358a63ce.chunk.js"
+  },
+  {
+    "revision": "8a1e26241b12e11c6cb67644b2ed2234",
+    "url": "/sample-defi-app/static/js/2.358a63ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f47f5b629f9198a09fce",
+    "url": "/sample-defi-app/static/js/3.937dc1a2.chunk.js"
+  },
+  {
+    "revision": "38af8bdbc21557fdf598",
+    "url": "/sample-defi-app/static/js/main.857dd486.chunk.js"
+  },
+  {
+    "revision": "377fe663ab078e501304",
+    "url": "/sample-defi-app/static/js/runtime-main.37b2022f.js"
+  }
+]);
